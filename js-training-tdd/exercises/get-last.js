ecfaf92f54675b289dece9d18ed0ee5b24d12669
TheCats100 <61271886+TheCats100@users.noreply.github.com>
2020-03-18 11:44:12 +0100

    Add files via upload

---
+++ js-training-tdd/exercises/get-last.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/*
+ * Create a `getLast` function that takes an array or a string
+ * and return the last element.
+ *
+ * @notions Data-Structures, Get
+ */
+
+// Your code:
+function getLast(x){
+    return x[(x.length)-1]
+}
+
+//* Begin of tests
+const assert = require('assert');
+
+assert.strictEqual(getLast([ 2, 42 ]), 42);
+assert.strictEqual(getLast([ 'pouet', 4, true ]), true);
+assert.strictEqual(getLast([ getLast ]), getLast);
+assert.strictEqual(getLast('salut'), 't');
+assert.strictEqual(getLast([]), undefined);
+// End of tests */
